acc987b7a3866d8e6800f6ab07f5e1edcdab856e
Tyler Gaisford <tyler.gaisford@gmail.com>
2023-03-02 20:23:20 -0700

    added variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -5,6 +5,32 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+            //Declairing name
+            string dogName = "Einstien";
+
+            int einstienAge = 5;
+
+            char firstLetter = 'E';
+
+            bool likesFetch = true;
+
+            double dogWeight = 33.3;
+
+            decimal tailHeight = 3.333m;
+
+            string unitsUsed = "inches";
+
+            Console.WriteLine($"{dogName} has a nub of a tail standing at {tailHeight}{unitsUsed} tall");
+
+
+
+
+
+
+
+
+
         }
     }
 }
